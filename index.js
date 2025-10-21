818e903982a6509d03c55706bc1ddfed4a3a48f8
Krish-006-19 <krish.ramena24@vit.edu>
2025-10-21 17:10:30 +0530

    latest changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://127.0.0.1:27017/Todo")
   .then((_) => console.log("mongo connected!"))
-  .catch((err) => console.err(err));
+  .catch((err) => console.error(err));
 app.use(require("cors")());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
