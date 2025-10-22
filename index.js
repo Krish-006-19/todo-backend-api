7e66b5240a56b4113b41cecc22427410408254d9
Krish Ramena <krish.ramena24@vit.edu>
2025-10-22 22:15:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/Todoapp")
   .then(() => console.log("Mongo connected!"))
   .catch((err) => console.error(err));
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(require("cors")({}));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cookieParser());
+app.use(require("cookie-parser")());
 
 app.use("/", require("./routes/auth"));
 app.use("/todo", require("./routes/todo"));
 
-app.listen(3000, () => console.log(`Server running on port 3000`));
+app.listen(3000, _ => console.log(`Server running on port 3000`));
